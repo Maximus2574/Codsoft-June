776078977dd1c8bd024ef73d9c214a6793b1b202
Maximus2574 <kshitijh25704@gmail.com>
2023-09-12 22:27:12 +0530

    initial commit

---
+++ Password Generator/Passgen.py
@@ -0,0 +1,31 @@
+import tkinter
+from tkinter import *
+
+root=Tk()
+root.config(bg="gray20")
+
+passwordLabel=Label(root,text="Password Generator",font=("times new roman",20,"bold"),bg="gray20",fg="white")
+passwordLabel.grid()
+
+
+
+
+
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
